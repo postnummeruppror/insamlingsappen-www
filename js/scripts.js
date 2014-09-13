ece372e5dcca91cbc65c26d466694250ed39a5f6
Peter Krantz <peter@peterkrantz.se>
2014-09-13 15:57:12 +0200

    fix chrome bug

--- js/scripts.js
+++ js/scripts.js
@@ -23,6 +23,41 @@ $(document).ready(function() {
 });
 
 
+function locate() {
+
+$("#postalCode").focus();
+  var map = L.map('map');
+
+  L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
+    maxZoom: 18,
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+    '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+    'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    id: 'examples.map-i86knfo3'
+  }).addTo(map);
+
+  map.on('locationfound', onLocationFound);
+  map.on('locationerror', onLocationError);
+
+  map.locate({setView: true, maxZoom: 16});
+}
+
+function onLocationFound(e) {
+
+  $("#latitude").val(e.latlng.lat);
+  $("#longitude").val(e.latlng.lng);
+  $("#accuracy").val(e.accuracy);
+
+  var radius = e.accuracy / 2;
+  L.marker(e.latlng).addTo(map).bindPopup("Din plats").openPopup();
+  L.circle(e.latlng, radius).addTo(map);
+
+}
+
+function onLocationError(e) {
+  alert(e.message);
+}
+
 
 
 var guid = (function() {
